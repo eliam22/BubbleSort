bb25d69c3fec3407fe27172939b6f0bede276a9f
eliam22 <49135431+eliam22@users.noreply.github.com>
2023-11-01 08:58:18 -0600

    Add files via upload

---
+++ TreeNode.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Actividad_10;
+
+/**
+ *
+ * @author eliamyaet
+ */
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+    int height;
+
+    public TreeNode(int data) {
+        this.data = data;
+        this.height = 1;
+    }
+}
+
