6ccf846d8c67c88c1eb56ec1cfc8b154ebb205b4
eliam22 <49135431+eliam22@users.noreply.github.com>
2023-11-03 08:55:35 -0600

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -12,7 +12,7 @@ public class BubbleSort {
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
         int n = arr.length;
 
-        // Tu algoritmo de ordenamiento Bubble Sort
+        // Algoritmo de ordenamiento Bubble Sort
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
